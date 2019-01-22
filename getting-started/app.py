42d2dd090e08bfc28be6f5015b8341d746ce695d
wisani khosa <wizro@gmx.com>
2019-01-22 23:57:45 +0200

    Reusable Components

--- getting-started/app.py
+++ getting-started/app.py
@@ -5,49 +5,41 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv("https://data.code4sa.org/api/views/7y3u-atvk/rows.csv?accessType=DOWNLOAD")
+df = df.drop(columns='Reasonforprotest')
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+        
+        #Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 colors = {
-    'background': '#111111',
+    'background': '#A1B1C1',
     'text': '#7fDBFF'
 }
 
 
 # creating app layout
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(children='Hello Dash', 
+app.layout = html.Div(children=[
+    html.H4(children='SA Protest Data 2013-2014', 
            style={'textAlign': 'center',
                  'color': colors['text']
                  }
            ),
+    generate_table(df)])
     
-    html.Div(children='''Dash: A web application framework for Python.''', style={
-        'textAlign' : 'center',
-        'color' : colors['text']
-    }),
-    
-    dcc.Graph(
-        id="example-graph-2",
-        figure={
-            'data': [
-                {'x' : [1, 2, 3], 'y' : [4, 1, 2], 'type' : 'bar', 'name' : 'SF'},
-                {'x' : [1, 2, 3], 'y' : [2, 4, 5], 'type' : 'bar', 'name' : u'Montreal'}
-            ],
-            'layout': {
-                'plot_bgcolor' : colors['background'],
-                'paper_bgcolor' : colors['background'],
-                'font' : {
-                    'color' : colors["text"]
-                },
-                'title': 'Dash Data Visualization'
-            }
-        })
-])
-
 # if running app gives port number in use change port to a different portn no
 if __name__ == '__main__':
 	app.run_server(debug=True, port='8080')
-

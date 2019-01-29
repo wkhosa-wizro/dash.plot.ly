ecbed9a7a04643c0ff8905386850f087f4574978
wisani khosa <wizro@gmx.com>
2019-01-29 18:25:37 +0200

    final commit

--- getting-started/app.py
+++ getting-started/app.py
@@ -6,9 +6,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import plotly.graph_objs as go
 
-df = pd.read_csv("https://data.code4sa.org/api/views/7y3u-atvk/rows.csv?accessType=DOWNLOAD")
-df = df.drop(columns='Reasonforprotest')
+df=pd.read_csv("https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv")
+#df = df.drop(columns='Reasonforprotest')
 
 def generate_table(dataframe, max_rows=10):
     return html.Table(
@@ -32,14 +33,34 @@ colors = {
 
 
 # creating app layout
-app.layout = html.Div(children=[
-    html.H4(children='SA Protest Data 2013-2014', 
-           style={'textAlign': 'center',
-                 'color': colors['text']
-                 }
-           ),
-    generate_table(df)])
-    
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size' : 15,
+                        'line': {'width' : 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout' : go.Layout(
+                xaxis={'type': 'log', 'title' : 'GDP Per Capita'},
+                yaxis={'title' : 'Life Expectancy'},
+                margin={'l' : 40, 'b' : 40, 't': 10, 'r' : 10},
+                legend={'x' : 0, 'y' : 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
 # if running app gives port number in use change port to a different portn no
 if __name__ == '__main__':
 	app.run_server(debug=True, port='8080')
